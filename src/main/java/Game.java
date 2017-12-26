f9d752a133ab14337c4915a5a3e0a5ad580d7f09
cbcwestwolf <1004626265@qq.com>
2017-12-26 17:17:41 +0800

    Initial commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,12 @@
+/**
+ * Created by cbcwestwolf on 2017/12/26.
+ */
+public class Game {
+    public static void main(String[] arg){
+        System.out.println("Get!");
+    }
+
+    public Game(){
+        System.out.println("Get!");
+    }
+}
